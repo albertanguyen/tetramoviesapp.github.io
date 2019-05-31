0c6747c6382835b913237622216e01b02b6189dd
albertanguyen <jaynguyen2@outlook.com>
2019-05-31 09:22:53 +0700

    FIrst commit. Pass data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
+import Rendermovies from './rendermovies';
+import './index.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: [],
+            search: '',
+            page: 1,
+            isloaded: false
+        }
+    }
+
+    componentDidMount() {
+        this.getmovies()
+    }
+
+    async getmovies() {
+        const url = "https://api.themoviedb.org/3/trending/all/day?language=en-US&&sort_by=created_at.asc&api_key=71a501ed9edf15977687b58ea874ff93";
+        let req = await fetch(url);
+        let jsonData = await req.json();
+
+        this.setState(
+            {
+                movies: jsonData.results,
+                page: jsonData.page,
+                isloaded: true
+            })
+        }
+
+    //genres ID list https://api.themoviedb.org/3/genre/tv/list?api_key=<<api_key>>&language=en-US&api_key=71a501ed9edf15977687b58ea874ff93
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="text-uppercase">TetraMovies app</h1>
+                </div>
+                <div className="App-body">
+                    {
+                    this.state.movies.map( (movie) => {
+                      console.log(movie)
+                    return <Rendermovies {...movie} />  
+                    })
+                    }
+                </div>
+                <div className="App-footer"></div>
+            </div>
+        )
+    }
 }
+
+
 
 export default App;
---
+++ src/rendermovies.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { Card, CardColumns } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import './App.css';
+// import { tsConstructorType } from '@babel/types';
+
+class Rendermovies extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isloaded: true,
+        }
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <CardColumns className="my-5 mx-5">
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>{this.props.original_title }</Card.Title>
+                        <Card.Text>{this.props.overview}</Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card className="p-3">
+                    <blockquote className="blockquote mb-0 card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.
+                     </p>
+                        <footer className="blockquote-footer">
+                            <small className="text-muted">
+                                Someone famous in <cite title="Source Title">Source Title</cite>
+                            </small>
+                        </footer>
+                    </blockquote>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional content.{' '}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card bg="primary" text="white" className="text-center p-3">
+                    <blockquote className="blockquote mb-0 card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.
+                    </p>
+                        <footer className="blockquote-footer">
+                            <small className="text-muted">
+                                Someone famous in <cite title="Source Title">Source Title</cite>
+                            </small>
+                        </footer>
+                    </blockquote>
+                </Card>
+                <Card className="text-center">
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional content.{' '}
+                        </Card.Text>
+                        <Card.Text>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img src="holder.js/100px160" />
+                </Card>
+                <Card className="text-right">
+                    <blockquote className="blockquote mb-0 card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.
+                    </p>
+                        <footer className="blockquote-footer">
+                            <small className="text-muted">
+                                Someone famous in <cite title="Source Title">Source Title</cite>
+                            </small>
+                        </footer>
+                    </blockquote>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This card has even longer content than the first to
+                            show that equal height action.
+                    </Card.Text>
+                        <Card.Text>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardColumns>
+        )
+    }
+
+}
+
+
+export default Rendermovies;
